1a9dc204a5e91bb0fa0c7ed885d9e85711f6b25b
mimi-kim-01 <mimi010912@g.skku.edu>
2022-03-24 01:00:31 +0900

    timeout

--- 1233.py
+++ 1233.py
@@ -1,16 +1,11 @@
 import sys
 
 S1, S2, S3 = map(int, sys.stdin.readline().split())
-sum_list = []
-max_list = []
+sum = [0]*81
 
 for i in range(1, S1 + 1):
     for j in range(1, S2 + 1):
         for k in range(1, S3 + 1):
-            sum_list.append(i + j + k)
+            sum[i + j + k] += 1
 
-max_list = [i for i in sum_list if sum_list.count(i) == max(sum_list, key = sum_list.count)]
-max_list = list(set(max_list))
-max_list.sort()
-
-print(max_list[0])
+print(sum.index(max(sum)))
