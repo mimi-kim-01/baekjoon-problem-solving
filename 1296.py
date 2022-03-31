63aec66454ee9240b60fcbe81780bde2154832f5
mimi-kim-01 <mimi010912@g.skku.edu>
2022-04-01 00:06:28 +0900

    complete

--- 1296.py
+++ 1296.py
@@ -5,7 +5,6 @@ def winning_rate(word):
     V = word.count('V')
     E = word.count('E')
     rate = ((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100
-    print(rate)
     return rate
 
 name = input()
@@ -15,7 +14,6 @@ for i in range(T):
     a = input()
     words.append(a)
 words.sort() #to alphabetical order
-print(words)
 for i in range(len(words)):
     rates.append(winning_rate(words[i]))
 maxrate = max(rates)
---
+++ 1333.py
@@ -0,0 +1,12 @@
+def time(N, L, D):
+    for i in range(N):
+        for j in range(((L+5)*N)//D + 1):
+            if (L+(L+5)*i <= D*j <= (L+5)*(i+1)):
+                return (D*j)  
+    return -1
+
+N, L, D = map(int, input().split())
+
+if time(N, L, D) != -1:
+    print(time(N, L, D))
+else: print((((L+5)*N)//D + 1)*D)
